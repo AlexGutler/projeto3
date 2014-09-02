f0b81bbbc19cd704e65df50084ff3f97b38214fe
Alex Gutler <alex11jgt@hotmail.com>
2014-09-02 20:35:52 -0300

    envio para tirar duvidas

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $connection = new \PDO("mysql:host=localhost;dbname=projeto3","root","00fb00");
+    $connection = new \PDO("mysql:host=localhost;dbname=projeto3","admin","admin");
 
 } catch (\PDOException $e){
     echo "Ocorreu um erro ao tentar a conexão com o banco de dados."."<br>Código: ".$e->getCode()."<br> Erro:".$e->getMessage();
